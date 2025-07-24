363ae0d590d0a574481b463f0fe7c716e1465ade
riley-1995 <rileydenn@gmail.com>
2025-07-24 14:44:11 -0700

    Update scripts sandbox paths and sbatch parameters

--- scripts/run-cross_validation_experiment-gpu-shared.sh
+++ scripts/run-cross_validation_experiment-gpu-shared.sh
@@ -4,9 +4,9 @@
 #SBATCH --account=cso100
 #SBATCH --partition=gpu-shared
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=10
-#SBATCH --cpus-per-task=1
-#SBATCH --mem=10G
+#SBATCH --ntasks-per-node=4
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=32G
 #SBATCH --gpus=1
 #SBATCH --time=04:00:00
 #SBATCH --output=%x.o%j.%N
@@ -17,8 +17,14 @@ module purge
 module load "${SINGULARITY_MODULE}"
 module list
 
+# Check if model type argument is passed
+if [ -z "$1" ]; then
+    echo "Error: No model type specified. Usage: sbatch $0 <cnn|rnn>"
+    exit 1
+fi
+
 MODEL_TYPE=$1  # Capture model type argument
 
 export NVIDIA_DISABLE_REQUIRE=true
 
-time -p singularity exec --bind /expanse,/scratch --nv ./train-container-sandbox python -u ./cross_validation_experiment.py --model rnn
+time -p singularity exec --bind /expanse,/scratch --nv ../sandbox python -u ./cross_validation_experiment.py --model "$MODEL_TYPE"
